0a89d8fbbf0182176c566fe9d753230b165c0731
SaiPrasad017 <3201237@spsd.sk.ca>
2024-10-08 14:38:21 -0600

    test

---
+++ 00 first project/sketch.js
@@ -0,0 +1,18 @@
+// Project Title
+// Your Name
+// Date
+// one line description
+
+//interactive program
+
+
+function setup() {
+  //this happends one at the start
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  //happened over and over (60fps)
+  background(220);
+  circle(mouseX, mouseY, 30);
+}
