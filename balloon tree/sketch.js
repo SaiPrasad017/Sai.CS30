3e018b954e5078fe1c4b26d9052ed7ad95f8ef99
SaiPrasad017 <3201237@spsd.sk.ca>
2024-11-15 15:10:31 -0600

    finnished the main features

--- balloon tree/sketch.js
+++ balloon tree/sketch.js
@@ -4,14 +4,15 @@
 
 let scale = 15;
 let seed;
+// let levelOfLeaves;
 
 function setup() {
   createCanvas(500, 500);
-  background(255);
   seed = random(100);
 }
 
 function draw() {
+  background(255);
   drawTree(width/2, height*0.9, 90, 6);
   randomSeed(seed);
 }
@@ -25,21 +26,25 @@ function drawTree(x1, y1, angle, depth) {
   if (depth > 0) {
     let x2 = x1 + (cos(radians(angle))*depth*scale); //calculate endpoints of current branch
     let y2 = y1 - (sin(radians(angle))*depth*scale); //using trig ratios. Get shorter based on depth
+    let treeAngle = map(mouseX, 0, width, 0, 30);
     strokeWeight(depth - 1);
     drawLine(x1, y1, x2, y2, depth);
     //for a 2-branch tree:
-    drawTree(x2, y2, angle-18, depth-1);
-    drawTree(x2, y2, angle+18, depth-1);
+    drawTree(x2, y2, angle-treeAngle, depth-1);
+    drawTree(x2, y2, angle+treeAngle, depth-1);
     //3-branch Tree
     drawTree(x2, y2, angle, depth-1);
-    if(depth < 5){
-      drawLeaf(x2,y2,depth*random(5,10));
+    if(keyCode === 88){
+      if(depth < 5){
+        drawLeaf(x2,y2,depth*random(5,10));
+        // levelOfLeaves = 5;
+        // return levelOfLeaves;
+      }
     }
   }
 }
 
 function drawLeaf(x2, y2, depth){
-  //di = random(10,20);
   fill(random(255),random(255),random(255));
   circle(x2,y2,depth);
 }
