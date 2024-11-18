f57e368bb6a186072cab051b30cd0f4cc9f8ce75
SaiPrasad017 <3201237@spsd.sk.ca>
2024-11-18 14:43:14 -0600

    finnished balloon tree

--- balloon tree/sketch.js
+++ balloon tree/sketch.js
@@ -2,12 +2,14 @@
 //sai
 //nov 13
 
+//seting global varibles
 let scale = 15;
 let seed;
-// let levelOfLeaves;
+let leavesScale = 0;
 
 function setup() {
   createCanvas(500, 500);
+  //creating set seed
   seed = random(100);
 }
 
@@ -29,22 +31,37 @@ function drawTree(x1, y1, angle, depth) {
     let treeAngle = map(mouseX, 0, width, 0, 30);
     strokeWeight(depth - 1);
     drawLine(x1, y1, x2, y2, depth);
+
     //for a 2-branch tree:
     drawTree(x2, y2, angle-treeAngle, depth-1);
     drawTree(x2, y2, angle+treeAngle, depth-1);
+
     //3-branch Tree
     drawTree(x2, y2, angle, depth-1);
-    if(keyCode === 88){
-      if(depth < 5){
-        drawLeaf(x2,y2,depth*random(5,10));
-        // levelOfLeaves = 5;
-        // return levelOfLeaves;
-      }
+
+    //drawing leaves
+    if(depth < leavesScale){
+      drawLeaf(x2,y2,depth*random(5,10));
     }
   }
 }
 
+function keyPressed(){
+  //adding leaves
+  if(keyCode === 88){
+    leavesScale = leavesScale + 1;
+    return leavesScale;
+  }
+
+  //destroying leaves
+  if(keyCode === 90){
+    leavesScale = leavesScale - 1;
+    return leavesScale;
+  }
+}
+
 function drawLeaf(x2, y2, depth){
+  //drawing leaves
   fill(random(255),random(255),random(255));
   circle(x2,y2,depth);
 }
